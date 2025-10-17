c46190bd854516f1e121736a0e036d31d825fb37
Timothée <83133135+timlapov@users.noreply.github.com>
2025-10-17 15:26:00 +0200

    Add tests to verify context loading and repository bean availability
    - Added assertions in `contextLoads` test to ensure Spring context is initialized.
    - Introduced `allRepositoriesAreLoaded` test to verify required beans (`UserRepository`, `LocationRepository`, `StationRepository`) are correctly loaded into the application context.

--- src/test/java/art/lapov/vavapi/VavapiApplicationTests.java
+++ src/test/java/art/lapov/vavapi/VavapiApplicationTests.java
@@ -1,17 +1,36 @@
 package art.lapov.vavapi;
 
+import art.lapov.vavapi.repository.LocationRepository;
+import art.lapov.vavapi.repository.StationRepository;
+import art.lapov.vavapi.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class VavapiApplicationTests {
 
+    @Autowired
+    private ApplicationContext context;
+
+
     @Test
     void contextLoads() {
+        assertNotNull(context);
+    }
+
+    @Test
+    void allRepositoriesAreLoaded() {
+        assertNotNull(context.getBean(UserRepository.class));
+        assertNotNull(context.getBean(LocationRepository.class));
+        assertNotNull(context.getBean(StationRepository.class));
     }
 
 }
